5ce7f0f4834c1f0b96fe2d8b68f290c1d94534db
Vitorvgc <vitorvgc07@gmail.com>
2017-02-14 09:54:25 -0300

    Implement delegate and dataSource of Collection Views

--- BepidModelCanvas/ViewController.swift
+++ BepidModelCanvas/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet var blocks: [UICollectionView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.blocks.forEach {
+            $0.delegate = self
+            $0.dataSource = self
+            $0.backgroundColor = UIColor(red: 197/255.0, green: 221/255.0, blue: 1, alpha: 1)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK - CollectionView Data Source
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let postitCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostitCell", for: indexPath) as! PostitCell
+        
+        postitCell.titleTextField.text = "title \(indexPath.row)"
+        postitCell.backgroundColor = .blue
+        
+        return postitCell
     }
 
-
+    //MARK: CollectionView Delegate Flow Layout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.frame.size.width * 0.85
+        return CGSize(width: width, height: width / 4)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let cellWidth = collectionView.frame.width * 0.85
+        let cellHeight = cellWidth / 4
+        
+        let verticalInset = cellHeight * 0.25
+        let horizontalInset = cellWidth * 0.25
+        
+        return UIEdgeInsets(top: verticalInset, left: horizontalInset, bottom: verticalInset, right: horizontalInset)
+    }
+    
 }
 
