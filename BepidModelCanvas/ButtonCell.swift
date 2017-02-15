642f2e1d6842afab7b1515fb158ca0e98f7d78e0
Vitorvgc <vitorvgc07@gmail.com>
2017-02-15 11:46:47 -0300

    Add button cell

---
+++ BepidModelCanvas/ButtonCell.swift
@@ -0,0 +1,86 @@
+//
+//  ButtonCell.swift
+//  BepidModelCanvas
+//
+//  Created by Vítor Chagas on 15/02/17.
+//  Copyright © 2017 BepidCanvas. All rights reserved.
+//
+
+import UIKit
+
+class ButtonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var onSelection: () -> Void = {}
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = 20
+        self.backgroundColor = .white
+        
+        self.imageView.image = UIImage(named: "plus")
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognizer:)))
+        tapGestureRecognizer.allowedPressTypes = [NSNumber(value: UIPressType.select.rawValue)]
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func resizeOutlets() {
+        
+        let width = self.frame.size.width
+        let height = self.frame.size.height
+        
+        self.imageView.frame = CGRect(x: width * 0.1, y: height * 0.1, width: width * 0.8, height: height * 0.8)
+    }
+    
+    func handleTap(gestureRecognizer: UITapGestureRecognizer) {
+        self.onSelection()
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        if context.nextFocusedItem === self {
+            
+            coordinator.addCoordinatedAnimations({
+                
+                self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+                
+                self.layer.shadowOpacity = 0.2
+                self.layer.shadowOffset = CGSize(width: 0, height: 15)
+                self.layer.shadowRadius = 15
+                
+                self.addMotionEffect(self.motionEffectGroup)
+                
+            }, completion: nil)
+        }
+        else if context.previouslyFocusedItem === self {
+            
+            coordinator.addCoordinatedAnimations({
+                
+                self.transform = CGAffineTransform.identity
+                self.layer.shadowOpacity = 0
+                self.removeMotionEffect(self.motionEffectGroup)
+                
+            }, completion: nil)
+        }
+    }
+    
+    lazy private var motionEffectGroup: UIMotionEffectGroup = {
+        
+        let horizontalAxisMotionEffect = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        horizontalAxisMotionEffect.minimumRelativeValue = -6.0
+        horizontalAxisMotionEffect.maximumRelativeValue = 6.0
+        
+        let verticalAxisMoveEffect = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        verticalAxisMoveEffect.minimumRelativeValue = -6.0
+        verticalAxisMoveEffect.maximumRelativeValue = 6.0
+        
+        let motionEffectGroup = UIMotionEffectGroup()
+        motionEffectGroup.motionEffects = [horizontalAxisMotionEffect, verticalAxisMoveEffect]
+        
+        return motionEffectGroup
+    }()
+
+}
