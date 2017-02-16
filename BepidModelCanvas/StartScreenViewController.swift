0cc9393d42d895f657ab10082098cdd904261dfa
Renata Faria <renata.faria.gomes@hotmail.com>
2017-02-16 15:22:37 -0300

    tela inicial adicionada junto com a segue para mudança de tela

---
+++ BepidModelCanvas/StartScreenViewController.swift
@@ -0,0 +1,85 @@
+//
+//  StartScreenViewController.swift
+//  telainicialtvos
+//
+//  Created by Renata Faria on 16/02/17.
+//  Copyright © 2017 Renata Faria. All rights reserved.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController, UICollectionViewDelegateFlowLayout,UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var CanvaImage: UIImageView!
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "teste", for: indexPath) as! CanvasModelsCollectionViewCell
+        if indexPath.row == 0 {
+            self.CanvaImage.image = #imageLiteral(resourceName: "newCanvasDemo")
+            cell.CanvaImage.image = #imageLiteral(resourceName: "newCanvasDemo")
+            cell.CanvaTitle.text! = "Add new canvas"
+        }
+        
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 60
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 48, 0, 48)
+         //return UIEdgeInsetsMake(0, 0, 0, 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 435, height: 315)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didUpdateFocusIn context: UICollectionViewFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        ///AQUI MUDAMOS A IMAGEM DO FUNDO PARA SER A IMAGEM DO CANVAS MAS DA UM ERRO NA HORA DE TROCAR DA SEGUE
+        
+        //let cell = collectionView.cellForItem(at: context.nextFocusedIndexPath!) as! CanvasModelsCollectionViewCell
+            //self.CanvaImage.image = cell.CanvaImage.image
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! CanvasModelsCollectionViewCell
+        performSegue(withIdentifier: "OpenCanvas", sender: cell)
+    }
+    
+    
+    ///ADICIONAR DADOS IMPORTANTES PARA A NAVEGAÇÃO
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using [segue destinationViewController].
+//        // Pass the selected object to the new view controller.
+//    }
+
+}
