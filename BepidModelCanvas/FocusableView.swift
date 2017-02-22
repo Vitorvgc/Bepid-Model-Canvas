525e0c8c0b03a7e87f87172193c8090f6ee6ac22
Vitorvgc <vitorvgc07@gmail.com>
2017-02-22 11:57:40 -0300

    Add FocusableView class

---
+++ BepidModelCanvas/FocusableView.swift
@@ -0,0 +1,60 @@
+//
+//  FocusableView.swift
+//  BepidModelCanvas
+//
+//  Created by Vítor Chagas on 22/02/17.
+//  Copyright © 2017 BepidCanvas. All rights reserved.
+//
+
+import UIKit
+
+class FocusableView: UIView {
+    
+    override var canBecomeFocused: Bool {
+        return true
+    }
+
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        if context.nextFocusedItem === self {
+            
+            coordinator.addCoordinatedAnimations({
+                
+                self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+                self.layer.shadowOpacity = 0.2
+                self.layer.shadowOffset = CGSize(width: 0, height: 15)
+                self.layer.shadowRadius = 15
+                self.addMotionEffect(self.motionEffectGroup)
+                
+            }, completion: nil)
+        }
+        else if context.previouslyFocusedItem === self {
+            
+            coordinator.addCoordinatedAnimations({
+                
+                self.transform = CGAffineTransform.identity
+                self.layer.shadowOpacity = 0
+                self.removeMotionEffect(self.motionEffectGroup)
+                
+            }, completion: nil)
+        }
+        
+    }
+    
+    lazy private var motionEffectGroup: UIMotionEffectGroup = {
+        
+        let horizontalAxisMotionEffect = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        horizontalAxisMotionEffect.minimumRelativeValue = -6.0
+        horizontalAxisMotionEffect.maximumRelativeValue = 6.0
+        
+        let verticalAxisMoveEffect = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        verticalAxisMoveEffect.minimumRelativeValue = -6.0
+        verticalAxisMoveEffect.maximumRelativeValue = 6.0
+        
+        let motionEffectGroup = UIMotionEffectGroup()
+        motionEffectGroup.motionEffects = [horizontalAxisMotionEffect, verticalAxisMoveEffect]
+        
+        return motionEffectGroup
+    }()
+    
+}
