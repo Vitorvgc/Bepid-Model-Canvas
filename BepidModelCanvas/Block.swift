ae06fe2fd60d7c9821ca8eaa06c7d21f8ea3fbf4
Vitorvgc <vitorvgc07@gmail.com>
2017-02-25 22:34:13 -0300

    Add static values of blocks

---
+++ BepidModelCanvas/Block.swift
@@ -0,0 +1,26 @@
+//
+//  Block.swift
+//  BepidModelCanvas
+//
+//  Created by Vítor Chagas on 25/02/17.
+//  Copyright © 2017 BepidCanvas. All rights reserved.
+//
+
+import UIKit
+
+extension Block {
+    
+    static let values: [(title: String, icon: UIImage)] =
+    [
+        ("Key Partners"          , #imageLiteral(resourceName: "heart")),
+        ("Key Activities"        , #imageLiteral(resourceName: "heart")),
+        ("Key Resources"         , #imageLiteral(resourceName: "heart")),
+        ("Customer Settings"     , #imageLiteral(resourceName: "heart")),
+        ("Channels"              , #imageLiteral(resourceName: "heart")),
+        ("Customer Relationships", #imageLiteral(resourceName: "heart")),
+        ("Value Propositions"    , #imageLiteral(resourceName: "heart")),
+        ("Cost Structure"        , #imageLiteral(resourceName: "heart")),
+        ("Revenue Streams"       , #imageLiteral(resourceName: "heart"))
+    ]
+    
+}
---
+++ BepidModelCanvas/BusinessModelCanvas.swift
@@ -0,0 +1,9 @@
+//
+//  BusinessModelCanvas.swift
+//  BepidModelCanvas
+//
+//  Created by Vítor Chagas on 25/02/17.
+//  Copyright © 2017 BepidCanvas. All rights reserved.
+//
+
+import Foundation
